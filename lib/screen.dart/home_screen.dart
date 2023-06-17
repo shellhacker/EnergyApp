6a0a2f2e341918209a8288b0dc8c9a726a0bcdd8
shabanas <shabanaspb@gmail.com>
2023-06-17 23:06:58 +0530

    refresher added

--- lib/screen.dart/home_screen.dart
+++ lib/screen.dart/home_screen.dart
@@ -38,103 +38,108 @@ class HomeScreen extends StatelessWidget {
         ),
         body: Consumer<HomeController>(
             builder: (context, homeController, widget) {
-          return SingleChildScrollView(
-              child: homeController.result.isEmpty
-                  ? Center(child: CircularProgressIndicator())
-                  : Column(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(10),
-                          child: ListView.builder(
-                            shrinkWrap: true,
-                            itemCount: homeController.result.length,
-                            itemBuilder: (context, index) {
-                              return Container(
-                                padding: EdgeInsets.all(10),
-                                child: Column(children: [
-                                  Container(
-                                    padding: EdgeInsets.all(8),
-                                    decoration: BoxDecoration(
-                                      gradient: LinearGradient(
-                                        colors: [Colors.purple, Colors.blue],
-                                        begin: Alignment.bottomLeft,
-                                        end: Alignment.topRight,
+          return RefreshIndicator(
+            onRefresh: () {
+              return homeController.fecthData(context);
+            },
+            child: SingleChildScrollView(
+                child: homeController.result.isEmpty
+                    ? Center(child: CircularProgressIndicator())
+                    : Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(10),
+                            child: ListView.builder(
+                              shrinkWrap: true,
+                              itemCount: homeController.result.length,
+                              itemBuilder: (context, index) {
+                                return Container(
+                                  padding: EdgeInsets.all(10),
+                                  child: Column(children: [
+                                    Container(
+                                      padding: EdgeInsets.all(8),
+                                      decoration: BoxDecoration(
+                                        gradient: LinearGradient(
+                                          colors: [Colors.purple, Colors.blue],
+                                          begin: Alignment.bottomLeft,
+                                          end: Alignment.topRight,
+                                        ),
+                                      ),
+                                      // color: Colors.amber,
+                                      // height: 60,
+                                      width: size.width / 1.2,
+                                      child: Column(
+                                        children: [
+                                          vSpaceMin,
+                                          Row(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.spaceBetween,
+                                            children: [
+                                              Column(
+                                                children: [
+                                                  vSpaceMin,
+                                                  Text(
+                                                    "\$ " +
+                                                        (homeController
+                                                            .result[index]
+                                                                ['NOK_per_kWh']
+                                                            .toString()),
+                                                    style: TextStyle(
+                                                        fontSize: 20,
+                                                        fontWeight:
+                                                            FontWeight.bold),
+                                                  ),
+                                                  vSpaceMin,
+                                                  Text("NOK_per_kWh"),
+                                                ],
+                                              ),
+                                              Column(
+                                                children: [
+                                                  vSpaceMin,
+                                                  Text(
+                                                    "\$ " +
+                                                        (homeController
+                                                            .result[index]
+                                                                ['EUR_per_kWh']
+                                                            .toString()),
+                                                    style: TextStyle(
+                                                        fontSize: 20,
+                                                        fontWeight:
+                                                            FontWeight.bold),
+                                                  ),
+                                                  vSpaceMin,
+                                                  Text("EUR_per_kWh"),
+                                                ],
+                                              ),
+                                            ],
+                                          ),
+                                          vSpaceMin,
+                                          Row(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.spaceBetween,
+                                            children: [
+                                              Text(
+                                                  "time_start : ${homeController.result[index]['time_start'].toString()}"),
+                                              Text(
+                                                  "time_end : ${homeController.result[index]['time_end'].toString()}")
+                                            ],
+                                          ),
+                                          vSpaceMin,
+                                          Text(
+                                              "EXR :${homeController.result[index]['EXR'].toString()} ")
+                                        ],
                                       ),
                                     ),
-                                    // color: Colors.amber,
-                                    // height: 60,
-                                    width: size.width / 1.2,
-                                    child: Column(
-                                      children: [
-                                        vSpaceMin,
-                                        Row(
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.spaceBetween,
-                                          children: [
-                                            Column(
-                                              children: [
-                                                vSpaceMin,
-                                                Text(
-                                                  "\$ " +
-                                                      (homeController
-                                                          .result[index]
-                                                              ['NOK_per_kWh']
-                                                          .toString()),
-                                                  style: TextStyle(
-                                                      fontSize: 20,
-                                                      fontWeight:
-                                                          FontWeight.bold),
-                                                ),
-                                                vSpaceMin,
-                                                Text("NOK_per_kWh"),
-                                              ],
-                                            ),
-                                            Column(
-                                              children: [
-                                                vSpaceMin,
-                                                Text(
-                                                  "\$ " +
-                                                      (homeController
-                                                          .result[index]
-                                                              ['EUR_per_kWh']
-                                                          .toString()),
-                                                  style: TextStyle(
-                                                      fontSize: 20,
-                                                      fontWeight:
-                                                          FontWeight.bold),
-                                                ),
-                                                vSpaceMin,
-                                                Text("EUR_per_kWh"),
-                                              ],
-                                            ),
-                                          ],
-                                        ),
-                                        vSpaceMin,
-                                        Row(
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.spaceBetween,
-                                          children: [
-                                            Text(
-                                                "time_start : ${homeController.result[index]['time_start'].toString()}"),
-                                            Text(
-                                                "time_end : ${homeController.result[index]['time_end'].toString()}")
-                                          ],
-                                        ),
-                                        vSpaceMin,
-                                        Text(
-                                            "EXR :${homeController.result[index]['EXR'].toString()} ")
-                                      ],
-                                    ),
-                                  ),
-                                  hSpaceMin,
-                                  vSpaceMedium,
-                                ]),
-                              );
-                            },
+                                    hSpaceMin,
+                                    vSpaceMedium,
+                                  ]),
+                                );
+                              },
+                            ),
                           ),
-                        ),
-                      ],
-                    ));
+                        ],
+                      )),
+          );
         }));
   }
 }
